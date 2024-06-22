50a8aeabd852d59b300085c5c44d59fd6a10310e
niclan051 <niclan052@gmail.com>
2024-06-22 09:00:37 +0300

    split the controller into 3 parts for each existing movement mechanic

--- Assets/Scripts/PlayerDashing.cs
+++ Assets/Scripts/PlayerDashing.cs
@@ -1,13 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerDashing : MonoBehaviour
 {
-    [Header("Regular movement")]
-    [SerializeField] private float horizontalSpeedMultiplier;
-    [SerializeField] private float jumpMultiplier;
-
-    [Header("Dashing")]
-    [SerializeField] private float dashTime; // in seconds
+    [SerializeField] private double dashSeconds;
     [SerializeField] private float dashForce;
     [SerializeField] private uint maxDashCount;
     private bool _dashing;
@@ -18,49 +14,70 @@ public class PlayerController : MonoBehaviour
     
     private GroundCollision _groundCollision;
     private Rigidbody2D _rb2D;
-    
+
     private void Start()
     {
         _groundCollision = GetComponentInChildren<GroundCollision>();
         _rb2D = GetComponent<Rigidbody2D>();
     }
+
     private void Update()
     {
         Vector2 soonToBeVelocity = _rb2D.velocity;
-        
-        // regular movement
-        soonToBeVelocity.x = Input.GetAxis("Horizontal") * horizontalSpeedMultiplier;
+
         if (_groundCollision.OnGround)
         {
-            _groundCollision.OnGround = false;
-            
-            if (Input.GetButton("Jump")) soonToBeVelocity.y = jumpMultiplier;
-
-            _dashesLeft = maxDashCount;
+            ResetDash();
         }
-
-        // dashing
+        
         if (!_dashing)
         {
             _dashDirection = new Vector2(Input.GetAxis("Dash Horizontal"), Input.GetAxis("Dash Vertical"));
             if (Input.GetButtonDown("Dash") && _dashesLeft > 0)
             {
-                _dashing = true;
-                _dashesLeft--;
-                _currentDashTime = 0;
+                StartDash();
             }
         }
         else
         {
-            soonToBeVelocity = _dashDirection * dashForce;
-            _currentDashTime += Time.deltaTime;
-            if (_currentDashTime >= dashTime)
+            UpdateDash(ref soonToBeVelocity);
+            if (_currentDashTime >= dashSeconds)
             {
-                _dashing = false;
+                EndDash();
                 soonToBeVelocity = _dashDirection;
             }
         }
 
         _rb2D.velocity = soonToBeVelocity;
+    }
+
+    private void StartDash()
+    {
+        _dashing = true;
+        _dashesLeft--;
+        _currentDashTime = 0;
+        _groundCollision.OnGround = false;
+    }
+    // ReSharper disable once RedundantAssignment
+    private void UpdateDash(ref Vector2 soonToBeVelocity)
+    {
+        soonToBeVelocity = _dashDirection * dashForce;
+        _currentDashTime += Time.deltaTime;
+    }
+    private void EndDash()
+    {
+        _dashing = false;
+    }
+
+    public void ResetDash(int count = -1)
+    {
+        if (count == -1)
+        {
+            _dashesLeft = maxDashCount;
+        }
+        else
+        {
+            _dashesLeft = Convert.ToUInt32(count);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerRegularMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerRegularMovement : MonoBehaviour
+{
+    [SerializeField] private float horizontalSpeedMultiplier;
+    
+    private Rigidbody2D _rb2D;
+
+    private void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Vector2 soonToBeVelocity = _rb2D.velocity;
+        soonToBeVelocity.x = Input.GetAxis("Horizontal") * horizontalSpeedMultiplier;
+        _rb2D.velocity = soonToBeVelocity;
+    }
+}
