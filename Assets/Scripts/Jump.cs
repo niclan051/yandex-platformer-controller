b3a62b66ed70866913f4c3b885adb400268b51d2
F1aki <159679197+F1aki@users.noreply.github.com>
2024-06-23 13:12:14 +0500

    Механика прыжка с платформ

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -31,19 +31,31 @@ public class Jump : MonoBehaviour
     }
     void Jump1()
     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            Invoke("IgnoreLayerOff", 0.5f);
+        }
+        
         if(Input.GetKeyDown(KeyCode.Space))
         {
             if(onGround)
             {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
                 rb.AddForce(Vector2.up * JumpForce);
             }
             else if (++jumpCount < maxJumpValue)
             {
+                
                 rb.AddForce(Vector2.up * JumpForce);
             }
         }
 
         if (onGround) { jumpCount = 0; }
+    }
+    void IgnoreLayerOff()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 }
 
